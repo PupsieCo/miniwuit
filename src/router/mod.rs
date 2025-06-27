9815ffe7741c6893a0a753a30d8fa8914000ea78
frizzy <frizzy@pupsie.co>
2025-06-27 02:24:22 +0000

    refactor: enhance router service management with explicit service types

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -12,7 +12,8 @@ extern crate conduwuit_core as conduwuit;
 use std::{panic::AssertUnwindSafe, pin::Pin, sync::Arc};
 
 use conduwuit::{Error, Result, Server};
-use conduwuit_service::Services;
+use conduwuit_service::Services as CoreServices;
+use conduwuit_social_service::Services as SocialServices;
 use futures::{Future, FutureExt, TryFutureExt};
 
 conduwuit::mod_ctor! {}
@@ -32,9 +33,10 @@ pub extern "Rust" fn start(
 
 #[unsafe(no_mangle)]
 pub extern "Rust" fn stop(
-	services: Arc<Services>,
+	core_services: Arc<CoreServices>,
+	social_services: Arc<SocialServices>,
 ) -> Pin<Box<dyn Future<Output = Result<()>> + Send>> {
-	AssertUnwindSafe(run::stop(services))
+	AssertUnwindSafe(run::stop(core_services, social_services))
 		.catch_unwind()
 		.map_err(Error::from_panic)
 		.unwrap_or_else(Err)
@@ -43,9 +45,10 @@ pub extern "Rust" fn stop(
 
 #[unsafe(no_mangle)]
 pub extern "Rust" fn run(
-	services: &Arc<Services>,
+	core_services: &Arc<CoreServices>,
+	social_services: &Arc<SocialServices>,
 ) -> Pin<Box<dyn Future<Output = Result<()>> + Send>> {
-	AssertUnwindSafe(run::run(services.clone()))
+	AssertUnwindSafe(run::run(core_services.clone(), social_services.clone()))
 		.catch_unwind()
 		.map_err(Error::from_panic)
 		.unwrap_or_else(Err)
